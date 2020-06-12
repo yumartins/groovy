c5f8cdfb8c0db6ca395d226e06c0122988d0c681
yumartins <yuri@estudioflow.com.br>
2020-06-12 09:40:55 -0300

    chore: eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,59 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    'eslint-plugin-import-helpers',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'import/no-extraneous-dependencies': 0,
+    camelcase: 0,
+    'jsx-a11y/anchor-is-valid': ['error', {
+      components: ['Link'],
+      specialLink: ['hrefLeft', 'hrefRight'],
+      aspects: ['invalidHref', 'preferButton'],
+    }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'space-before-function-paren': ['error', {
+      anonymous: 'always',
+      named: 'always',
+      asyncArrow: 'always',
+    }],
+    'space-unary-ops': [2, {
+      words: true,
+      nonwords: false,
+      overrides: {
+        '!': true,
+        '!!': true,
+      },
+    }],
+    'import-helpers/order-imports': ['warn', {
+      newlinesBetween: 'always',
+      groups: [
+        '/^react/',
+        'module',
+        '/^@shared/',
+        ['parent', 'sibling', 'index'],
+      ],
+      alphabetize: { order: 'asc', ignoreCase: true },
+    },
+    ],
+  },
+};
