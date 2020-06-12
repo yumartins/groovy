6ebd79da250d2fcef443d52e01bed891765c4fd8
yumartins <yuri@estudioflow.com.br>
2020-06-12 10:30:31 -0300

    feat: document next

--- packages/app-dash/src/pages/_app.js
+++ packages/app-dash/src/pages/_app.js
@@ -1,3 +1,25 @@
+import {
+  func,
+  array,
+  string,
+  number,
+  object,
+  objectOf,
+  oneOfType,
+} from 'prop-types';
+
 const App = ({ Component, pageProps }) => <Component {...pageProps} />;
+
+App.propTypes = {
+  Component: func,
+  pageProps: objectOf(oneOfType([
+    string, number, object, array,
+  ])),
+};
+
+App.defaultProps = {
+  Component: () => {},
+  pageProps: {},
+};
 
 export default App;
