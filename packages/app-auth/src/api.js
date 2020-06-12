f5c4b2f13c7c3e2198ab58d0d7e471f081d86c14
yumartins <yuri@estudioflow.com.br>
2020-06-12 11:55:41 -0300

    feat: api base url

---
+++ packages/app-auth/src/api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export const api = axios.create({
+  baseURL: 'https://api.spotify.com/v1/',
+});
+
+export const auth = axios.create({
+  baseURL: 'https://accounts.spotify.com/',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
---
+++ packages/app-auth/src/index.js
@@ -0,0 +1,4 @@
+export {
+  api,
+  auth,
+} from './api';
