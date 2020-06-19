657d876a9ad2b8988f7e1a4fce17b7e5232b2f7c
yumartins <yuri@estudioflow.com.br>
2020-06-18 21:30:36 -0300

    feat: init carousel home

---
+++ packages/groovy-dash/src/layouts/Carousel/index.js
@@ -0,0 +1,58 @@
+import {
+  func,
+  number,
+  object,
+  arrayOf,
+} from 'prop-types';
+
+import {
+  View,
+  Image,
+  Title,
+  Items,
+  Content,
+} from './styles';
+
+const Carousel = ({
+  items,
+  selected,
+  onSelected,
+}) => (
+  <View>
+    <Title>Trending New Hits</Title>
+
+    {items && items.map(({
+      id,
+      name,
+      images,
+      artists,
+    }, index) => (
+      <Items
+        key={id}
+        selected={selected === index}
+        data-active={index === selected}
+      >
+        <Content>
+          <span>{artists[0].name}</span>
+          <h1>{name}</h1>
+        </Content>
+
+        <Image src={images[0].url} alt="" />
+      </Items>
+    ))}
+  </View>
+);
+
+Carousel.propTypes = {
+  items: arrayOf(object),
+  selected: number,
+  onSelected: func,
+};
+
+Carousel.defaultProps = {
+  items: [{}],
+  selected: 0,
+  onSelected: () => {},
+};
+
+export default Carousel;
---
+++ packages/groovy-dash/src/layouts/Carousel/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const View = styled.div`
+  
+`;
+
+export const Title = styled.h5``;
+
+export const Items = styled.div``;
+
+export const Image = styled.img``;
+
+export const Content = styled.div``;
